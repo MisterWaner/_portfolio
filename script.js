e1ee741d6a8b0e673865fc26de29605b76061a71
Erwan Soulas <erwan.soulas@gmail.com>
2024-06-08 23:55:12 +0200

    4th commit: ended about section ui

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const navCloserIcon = document.querySelector("#nav-closer-icon");
 const nav = document.querySelector("nav");
 const links = document.querySelectorAll("nav ul li a");
 
+//Nav Toggler
 function toggleNav() {
     navOpenerIcon.classList.toggle("toggled");
     navCloserIcon.classList.toggle("toggled");
@@ -15,3 +16,21 @@ links.forEach((link) => {
 });
 
 navToggler.addEventListener("click", toggleNav)
+
+//About Modal
+const aboutModalOpener = document.querySelector("#about_modal_opener");
+const aboutModalCloser = document.querySelector("#about_modal_closer");
+const aboutModal = document.querySelector("#about_modal");
+const html = document.querySelector("html");
+
+aboutModalOpener.addEventListener("click", () => {
+    aboutModal.classList.add("show");
+    html.classList.add("no-scroll")
+})
+
+aboutModalCloser.addEventListener("click", () => {
+    aboutModal.classList.remove("show");
+    html.classList.remove("no-scroll")
+})
+
+
