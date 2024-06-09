46bf07f5b0ffcd7bc6549ded499d3a5c711e76ef
Erwan Soulas <erwan.soulas@gmail.com>
2024-06-09 12:30:21 +0200

    5th commit : restructure project folder, add php language

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Portfolio of Erwan Soulas, a french passionate web developer" />
+    <title>Erwan Portfolio</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <!--------------------- HEADER ----------------------->
+    <?php
+    include("src/components/Header/header.html");
+    ?>
+
+    <!--------------------- NAV ----------------------->
+    <?php include ("src/components/Nav/navigation.html"); ?>
+
+    <!--------------------- MAIN ----------------------->
+    <main>
+        <!------------------ HERO SECTION ------------------>
+        <?php include("src/components/Sections/hero-section.html"); ?>
+
+        <!------------------ ABOUT SECTION ------------------>
+        <?php include("src/components/Sections/about-section.html"); ?>
+        <!------------------ PROJECTS SECTION ------------------>
+        <section id="projects">
+            <div class="section-title">Mes Projects</div>
+            <article class="projects-container">
+                <div>Bleu</div>
+                <div>Blanc</div>
+                <div>Rouge</div>
+            </article>
+        </section>
+    </main>
+    <script src="script.js"></script>
+</body>
+
+</html>
