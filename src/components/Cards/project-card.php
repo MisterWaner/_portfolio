5219ddb2c9b6e8692585625aa701db51aa8bd927
Erwan Soulas <erwan.soulas@gmail.com>
2024-06-09 18:51:48 +0200

    7th commit: ended project section ui

--- src/components/Cards/project-card.php
+++ src/components/Cards/project-card.php
@@ -1,34 +1,65 @@
-<div class="project-card">
-    <div class="project-card-header">
-        <h3 class="project-card-title">Titre du projet</h3>
-        <p class="project-card-description">Description du projet</p>
-    </div>
-    <div class="project-card-content">
-        <div>
-            <img src="https://picsum.photos/seed/picsum/300/200" alt="">
+<?php
+function readJSONFile($filename)
+{
+    try {
+        $jsonContents = file_get_contents($filename);
+
+        if ($jsonContents === false) {
+            throw new Exception('Impossible de lire le fichier JSON');
+        }
+
+        $data = json_decode($jsonContents, true);
+
+        if ($data === null) {
+            throw new Exception('Le contenu du fichier JSON est invalide');
+        }
+
+        return $data;
+    } catch (Exception $e) {
+        echo 'Une erreur est survenue : ' . $e->getMessage();
+    }
+}
+
+$filename = 'src/lib/dummy-data.json';
+$data = readJSONFile($filename);
+?>
+
+<?php foreach ($data as $project) : ?>
+    <div class="project-card-container">
+        <div class="project-card">
+            <div class="project-card-header">
+                <h3 class="project-card-title"><?= $project['name'] ?></h3>
+                <p class="project-card-description"><?= $project['description'] ?></p>
+            </div>
+            <div class="project-card-content">
+                <div class="project-card-image-container">
+                    <img src="<?= $project['image'] ?>" alt="">
+                </div>
+                <div class="project-card-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptate ex fugit est quisquam, ut veritatis eligendi iure expedita corrupti nobis aliquam rem adipisci eos omnis earum ipsam? Reiciendis molestias tempora blanditiis earum omnis delectus eveniet, harum molestiae perferendis eligendi nesciunt hic officiis ipsa! Obcaecati tenetur officia voluptatum laboriosam minus.
+                    </p>
+                    <p class="project-card-tech">
+                        Technologies :
+                        <span><?= implode(', ', $project['tech']) ?></span>
+                    </p>
+                </div>
+            </div>
+            <div class="project-card-footer">
+                <a href="<?= $project['link'] ?>" target="_blank" class="project-card-link">
+                    <span class="project-card-link-icon">
+                        <img src="src/assets/icons/arrow-up-right-from-square-solid.svg" alt="">
+                    </span>
+                    <span class="project-card-link-text">
+                        Voir le projet
+                    </span>
+                </a>
+                <a href="/" target="_blank" class="project-card-link">
+                    <span class="project-card-link-icon">
+                        <img src="src/assets/icons/github.svg" alt="">
+                    </span>
+                    <span class="project-card-link-text">Github</span>
+                </a>
+            </div>
         </div>
-        <div class="project-card-text">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptate ex fugit est quisquam, ut veritatis eligendi iure expedita corrupti nobis aliquam rem adipisci eos omnis earum ipsam? Reiciendis molestias tempora blanditiis earum omnis delectus eveniet, harum molestiae perferendis eligendi nesciunt hic officiis ipsa! Obcaecati tenetur officia voluptatum laboriosam minus.
-            </p>
-            <p class="project-card-tech">
-                dfgsdgbfdfvbf
-            </p>
-        </div>
     </div>
-    <div class="project-card-footer">
-        <a href="/" target="_blank" class="project-card-link">
-            <span>
-                <img src="src/assets/icons/arrow-up-right-from-square-solid.svg" alt="">
-            </span>
-            <span>
-                Voir le projet
-            </span>
-        </a>
-        <a href="/" target="_blank" class="project-card-link">
-            <span>
-                <img src="src/assets/icons/github.svg" alt="">
-            </span>
-            <span>Github</span>
-        </a>
-    </div>
-</div>
+<?php endforeach; ?>
